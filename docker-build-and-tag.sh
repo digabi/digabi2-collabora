8747f3c91542408c57eb93e21cd07014797441b5
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2024-05-06 12:32:17 +0300

    Rename abitti2 to digabi2 also in build and release scripts

--- docker-build-and-tag.sh
+++ docker-build-and-tag.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-OLD_VERSION=$(git ls-remote --tags --refs --sort="v:refname" git@github.com:digabi/abitti2-collabora.git | tail -n1 | sed 's/.*\///' 2>/dev/null || echo "v0.0.0") 
+OLD_VERSION=$(git ls-remote --tags --refs --sort="v:refname" git@github.com:digabi/digabi2-collabora.git | tail -n1 | sed 's/.*\///' 2>/dev/null || echo "v0.0.0")
 
 docker build -t "863419159770.dkr.ecr.eu-north-1.amazonaws.com/collabora:$OLD_VERSION" .
